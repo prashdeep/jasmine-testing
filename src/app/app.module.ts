4792382fdc815bf9bd305cb890eb97d9f51b7678
pradeep <pradeep.kumar44@gmail.com>
2018-12-17 16:15:55 +0530

    committing the template form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,15 @@ import { NgModule } from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { FirstComponent } from './first/first.component';
+import { TemplateFormComponent } from './template-form/template-form.component';
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    FirstComponent
+    FirstComponent,
+    TemplateFormComponent
   ],
   imports: [
     BrowserModule,
