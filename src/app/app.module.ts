0f0a08b60e9c8cec62c0c679c05e2b60a9ed3349
pradeep <pradeep.kumar44@gmail.com>
2018-12-17 16:10:20 +0530

    committing the changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { FirstComponent } from './first/first.component';
 
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstComponent
   ],
   imports: [
     BrowserModule,
